8fd35087ab554f6ff815e34b02e741083db9c48f
Saah <saahloss@gmail.com>
2024-05-18 08:24:33 -0300

    Fixed stamina and health icons being switched

--- AlwaysShowBarValues/StatBoxes/MainBox.cs
+++ AlwaysShowBarValues/StatBoxes/MainBox.cs
@@ -8,8 +8,8 @@ namespace AlwaysShowBarValues.StatBoxes
     internal class MainBox : StatBox
     {
         public MainBox() : base(KeybindList.Parse("H"), "main") {
-            this.TopValue = new PlayerStat("Health", new Microsoft.Xna.Framework.Rectangle(0, 411, 16, 16), new Microsoft.Xna.Framework.Vector2(0, 0));
-            this.BottomValue = new PlayerStat("Stamina", new Microsoft.Xna.Framework.Rectangle(16, 411, 16, 16), new Microsoft.Xna.Framework.Vector2(0, 0));
+            this.TopValue = new PlayerStat("Health", new Microsoft.Xna.Framework.Rectangle(16, 411, 16, 16), new Microsoft.Xna.Framework.Vector2(0, 0));
+            this.BottomValue = new PlayerStat("Stamina", new Microsoft.Xna.Framework.Rectangle(0, 411, 16, 16), new Microsoft.Xna.Framework.Vector2(0, 0));
             IsValid = true;
         }
 
